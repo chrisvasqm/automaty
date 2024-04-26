0b35bfd1b9f6791374e97b507fb51b45d2f964e7
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-26 12:56:16 -0400

    Remove drawer for now

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,14 +2,12 @@ import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Container from './components/Container'
 import HStack from './components/HStack'
-import Drawer from './components/Drawer'
 
 const Layout = () => {
   return (
     <>
       <Navbar />
       <HStack>
-        <Drawer />
         <Container>
           <Outlet />
         </Container>
