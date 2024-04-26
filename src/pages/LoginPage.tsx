753e3f12d1026063b617f87604abbb3be88ee86f
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-26 13:54:00 -0400

    Reduce min  width

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
                 alignItems: 'center'
             }}>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <Card sx={{ minWidth: '350px' }} elevation={8}>
+                <Card sx={{ minWidth: '300px' }} elevation={8}>
                     <CardContent>
                         <Stack gap={2}>
                             <Typography
