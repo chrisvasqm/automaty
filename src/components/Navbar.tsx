444d75047933b54fb6198333fe0505bb619c9069
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-26 15:57:22 -0400

    Add GitHub link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,27 @@
-import { Card, Typography } from '@mui/material'
+import { Card, Typography, Box, IconButton } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav>
             <Card elevation={6} sx={{ padding: 2, borderRadius: 0 }}>
-                <Typography
-                    id='app-name'
-                    variant='h1'
-                    fontSize={24}>Automaty</Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                    }}>
+                    <Typography
+                        id='app-name'
+                        variant='h1'
+                        fontSize={24}>Automaty</Typography>
+                    <Link to="https://github.com/chrisvasqm/automaty" target='_blank'>
+                        <IconButton color='primary' size='small'>
+                            <GitHubIcon />
+                        </IconButton>
+                    </Link>
+                </Box>
             </Card>
         </nav>
     )
