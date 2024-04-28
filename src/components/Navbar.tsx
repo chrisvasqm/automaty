7683647e81b12a24c8417995d722a7f0190d2653
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-28 15:56:40 -0400

    Show menu icon on smaller devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
-import { Card, Typography, Box, IconButton } from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Box, Card, IconButton, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const theme = useTheme();
+    const isTablet = useMediaQuery(theme.breakpoints.up('md'))
+
     return (
         <nav>
             <Card elevation={6} sx={{ padding: 2, borderRadius: 0 }}>
@@ -12,10 +16,21 @@ const Navbar = () => {
                         alignItems: 'center',
                         justifyContent: 'space-between'
                     }}>
-                    <Typography
-                        id='app-name'
-                        variant='h1'
-                        fontSize={24}>Automaty</Typography>
+                    <Stack direction={'row'} alignItems={'center'} gap={2}>
+                        {
+                            !isTablet &&
+                            <IconButton>
+                                <MenuIcon />
+                            </IconButton>
+                        }
+                        <Typography
+                            id='app-name'
+                            variant='h1'
+                            fontSize={24}>
+                            Automaty
+                        </Typography>
+                    </Stack>
+
                     <Link to="https://github.com/chrisvasqm/automaty" target='_blank'>
                         <IconButton id='link-github' color='primary' size='small'>
                             <GitHubIcon />
