272ddfa648c13cd13d350f211935b8ae091d85f7
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-30 01:19:18 -0400

    Remove tailwindcss

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,6 +1,7 @@
 import ImportContactsIcon from '@mui/icons-material/ImportContacts';
 import LoginIcon from '@mui/icons-material/Login';
 import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { grey } from '@mui/material/colors';
 import { Link } from 'react-router-dom';
 
 const SidePanel = () => {
@@ -8,7 +9,7 @@ const SidePanel = () => {
         <Box
             sx={{ width: '100%', maxWidth: 200 }}>
             <List>
-                <Link to='/'>
+                <Link to='/' style={{ textDecoration: 'none', color: grey[700] }}>
                     <ListItem disablePadding id='menu-login'>
                         <ListItemButton>
                             <ListItemIcon>
@@ -19,7 +20,7 @@ const SidePanel = () => {
                     </ListItem>
                 </Link>
 
-                <Link to='/students'>
+                <Link to='/students' style={{ textDecoration: 'none', color: grey[700] }}>
                     <ListItem disablePadding id='menu-students'>
                         <ListItemButton>
                             <ListItemIcon>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
 import router from './routes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
