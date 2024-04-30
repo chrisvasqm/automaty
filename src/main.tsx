64b9c9a2ca81fd9292629cc5b0d5b7ac44809020
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-30 01:22:11 -0400

    Remove body margin

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes.tsx';
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
