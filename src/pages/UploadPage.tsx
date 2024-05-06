c606a0fb2310efa5ea779ece6125c76917cd822c
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-05 22:36:46 -0400

    Reformat code

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -6,20 +6,19 @@ import Contact from '../models/Contact';
 
 const UploadPage = () => {
   const [contacts, setContacts] = useState<Contact[]>([])
-  const { CSVReader } = useCSVReader();
+  const { CSVReader } = useCSVReader()
 
   const handleUpload = (results: any) => {
-    const removedHeaders = results.data
-      .slice(1);
+    const removedHeaders = results.data.slice(1)
     const data = removedHeaders.map((row: any) =>
     ({
       id: row[0],
       name: row[1],
       phone: row[2],
       email: row[3]
-    }));
+    }))
 
-    setContacts(data);
+    setContacts(data)
   }
 
   return (
