fd02e6bc712ff676a7b6b9dfac5327733b77f5a1
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-07 19:16:48 -0400

    Add validation

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,24 +1,56 @@
-import { Box, Button, Stack } from '@mui/material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import { useState } from 'react';
 import { useCSVReader } from 'react-papaparse';
+import { z } from 'zod';
 import ContactsTable from '../components/ContactsTable';
 import Contact from '../models/Contact';
 
+const schema = z.array(
+  z.object({
+    id: z.string(),
+    name: z
+      .string({ required_error: 'Name is required' })
+      .min(1, 'Name must be at least 1 character long'),
+    phone: z
+      .string({ required_error: 'Phone is required' })
+      .min(5, 'Phone must be greater than 5 characters long')
+      .max(20, 'Phone must be less than 20 characters long'),
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email()
+  })
+)
+
 const UploadPage = () => {
   const [contacts, setContacts] = useState<Contact[]>([])
+  const [errors, setErrors] = useState<string[]>([]);
+
   const { CSVReader } = useCSVReader()
 
-  const handleUpload = (results: any) => {
-    const removedHeaders = results.data.slice(1)
-    const data = removedHeaders.map((row: any) =>
-    ({
-      id: row[0],
-      name: row[1],
-      phone: row[2],
-      email: row[3]
-    }))
+  const handleUpload = (results: { data: Contact[] }) => {
+    // Remove all the headers (e.g. id, name, phone, email)
+    const rawData = results.data.slice(1)
 
-    setContacts(data)
+    // Extract the data from each row in the CSV
+    const contacts: Contact[] = rawData.map((row: any) => (
+      {
+        id: row[0],
+        name: row[1].trim(),
+        phone: row[2].trim(),
+        email: row[3].trim()
+      }
+    ));
+
+    const validation = schema.safeParse(contacts);
+    if (!validation.success) {
+      const { errors } = validation.error;
+
+      setErrors(errors.map(error => `Error: ${error.message.toLowerCase()}. Please check your CSV`));
+      return;
+    }
+
+    setErrors([]);
+    setContacts(contacts)
   }
 
   return (
@@ -48,7 +80,12 @@ const UploadPage = () => {
         </Button>
       </Stack>
 
-      <ContactsTable contacts={contacts} />
+      {/* When there's at least one validation error, render the errors. Otherwise, render the Contacts */}
+      {
+        errors.length > 0 ?
+          errors.map((error, index) => <Typography key={index} color={'red'}>{error}</Typography>) :
+          <ContactsTable contacts={contacts} />
+      }
     </Box>
   )
 }
