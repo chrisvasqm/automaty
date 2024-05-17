40090b79d9eb3b4f65daa0cc98f34abb85fde6f6
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-17 19:26:24 -0400

    Improve look and feel of the Error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,11 +1,19 @@
+import { Button, Stack, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const ErrorPage = () => {
     return (
         <>
-            <h1>Error</h1>
-            <p>Seems like you have lost your way.</p>
-            <Link to='/'><button>Go Home</button></Link>
+            <Navbar />
+
+            <Stack gap={2} height={'90vh'} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Typography variant='h3'>404 Not Found</Typography>
+                <Typography>Seems like you have lost your way.</Typography>
+                <Link to='/'>
+                    <Button variant='contained'>Go Home</Button>
+                </Link>
+            </Stack>
         </>
     )
 }
